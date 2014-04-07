e08c0be5039b63cd63235b142399630da99dd07f
Anurag Priyam <anurag08priyam@gmail.com>
2014-04-07 17:44:49 +0530

    Put spinner against a backdrop.
    * Remove spin.js - use font awesome's spinner.
    * Use Bootstrap modal for backdrop.
    
    Signed-off-by: Anurag Priyam <anurag08priyam@gmail.com>

--- www/app.js
+++ www/app.js
@@ -4,7 +4,6 @@ require.config({
         jquery:     'lib/jquery/jquery',
         bootstrap:  'lib/bootstrap/dist/js/bootstrap',
         contextmenu:'lib/bootstrap-contextmenu/bootstrap-contextmenu',
-        spinner:    'lib/spin.js/spin',
         moment:     'lib/moment/moment',
         angular:    'lib/angular/angular',
         ngCookies:  'lib/angular-cookies/angular-cookies',
@@ -91,10 +90,10 @@ require.config({
     }
 });
 
-require(['spinner']
-, function (Spinner) {
+require(['bootstrap', 'less!styles']
+, function () {
 
-    require(['underscore', 'jquery', 'angular', 'dojo/has', 'dojo/_base/sniff', 'bootstrap', 'ngCookies', 'ngAnimate', 'ngMoment']
+    require(['underscore', 'jquery', 'angular', 'dojo/has', 'dojo/_base/sniff', 'ngCookies', 'ngAnimate', 'ngMoment']
     , function (_, $, angular, has) {
 
         'use strict';
@@ -155,26 +154,7 @@ require(['spinner']
                                     // just doesn't work.
                 ];
 
-                var spinner = spinner || new Spinner({
-                    lines: 13, // The number of lines to draw
-                    length: 20, // The length of each line
-                    width: 10, // The line thickness
-                    radius: 30, // The radius of the inner circle
-                    corners: 1, // Corner roundness (0..1)
-                    rotate: 0, // The rotation offset
-                    direction: 1, // 1: clockwise, -1: counterclockwise
-                    color: '#000', // #rgb or #rrggbb or array of colors
-                    speed: 1, // Rounds per second
-                    trail: 60, // Afterglow percentage
-                    shadow: false, // Whether to render a shadow
-                    hwaccel: false, // Whether to use hardware acceleration
-                    className: 'spinner', // The CSS class to assign to the spinner
-                    zIndex: 2e9, // The z-index (defaults to 2000000000)
-                    top: 'auto', // Top position relative to parent in px
-                    left: 'auto' // Left position relative to parent in px
-                });
-                spinner.spin($('#spinner-overlay')[0]);
-
+                $('#spinner').modal();
                 require(files
                 , function (template, controller) {
 
@@ -183,7 +163,7 @@ require(['spinner']
                         compile(view.contents())(scope);
                         injector.invoke(controller, scope);
                     });
-                    spinner.stop();
+                    $('#spinner').modal('hide');
                 });
             };
 
